1a92b8228bcc95788cc71ebeac06e34d22fa48c1
Elon Li <elonisme@keemail.me>
2023-11-24 18:48:45 +0800

    update relying

--- relying.sh
+++ relying.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+# lsp-bridge requirements
 sudo pacman -S python-orjson python-six python-setuptools python-paramiko python-rapidfuzz
 yay -S python-epc python-setuptools
-
+# bash lsp-server
 sudo pacman -S bash-language-server
+# latex lsp-server
+sudo pacman -S texlab
+# python lsp-server
+sudo pacman -S pyright
+
+# org-download requirements
+sudo pacman -S xclip
+
+# pdf viewer
+sudo pacman -S evince
